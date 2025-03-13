84038ebc27269f98c29d1f5bb5e30791daf99722
fabio thomaz <fabio_thomaz@live.com>
2025-03-12 22:03:07 -0400

    servidor startando com função para start stream

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from typing import Union
-
 from fastapi import FastAPI
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "SRC")))
+from SRC.controller.stream_controller.stream_controller import router as stream_router
 
 app = FastAPI()
 
+app.include_router(stream_router)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/start_stream")
-async def start_stream():
-    return {"stream": "started"}
+
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
